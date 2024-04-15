4b5a88ccac62fbe1445c62d742817317cb001fdc
Abdelhamid Akhatar <akhatarabd@cy-tech.fr>
2024-04-15 12:43:10 +0200

    cygym & mazerunner

---
+++ CyGym/components/home.php
@@ -0,0 +1,12 @@
+<div class="accueil">
+    <h1>
+        Welcome to <span>CyGym</span> <br> From the smartest to the strongest.
+    </h1>
+    <p>Building strong brains and strong bodys since 1983 <br> </p>
+    <button>
+        Start as member
+    </button>
+    <button>
+        Start as coach
+    </button>
+</div>
---
+++ CyGym/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CyGym - Akhatar Abdelhamid</title>
+    <link rel="stylesheet" href="styles/main_style.css">
+</head>
+<body>
+    <?php
+        include 'components/header.php';
+        echo "<a href='connexionAdmin.php' class='admin'>Admin</a>";
+        include 'components/home.php'
+    ?>
+</body>
+</html>
