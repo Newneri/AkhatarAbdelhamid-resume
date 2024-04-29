a0423c6414f0803952fe0f3b1abd8a4101fa5838
Abdelhamid Akhatar <akhatarabd@cy-tech.fr>
2024-04-30 01:22:59 +0200

    search results

--- CyGym/requestsForms/resultatsRecherche.php
+++ CyGym/requestsForms/resultatsRecherche.php
@@ -14,12 +14,15 @@
     session_start();
     $_SESSION["pagename"] = "search-page";
     include '../components/header.php';
+    ?>
+    <div class='card card-coaches'> 
+        <h2>Lessons</h2>
+        <table>
+    <?php
     define('ROOT_PATH', dirname(__DIR__) . '/');
     $result = [];
     $tmp = 0;
     $path = ROOT_PATH . "donnees/cours.csv";
-    echo "<div class='card card-coaches'> <h2>Lessons</h2>";
-    echo "<table>\n\n";
     $f = fopen($path, "r");
     while (($line = fgetcsv($f, 1000, ",")) !== false) {
         if($tmp == 0){
@@ -40,10 +43,10 @@
         $tmp++;
     }
     fclose($f);
-    echo "\n</table>";
-    echo "</div>";
-    echo "<a class='btn-revenir' href='../index.php'> Revenir </a>";
     ?>
+    </table>
+    </div>
+    <a class='btn-revenir' href='../index.php'> Revenir </a>
 </body>
 
 </html>
